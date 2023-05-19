153604f00415c95e76471a36d7fd1be04bf6968a
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2023-05-19 16:08:58 +0300

    Stream API

--- server/src/managers/CommandReceiver.java
+++ server/src/managers/CommandReceiver.java
@@ -9,6 +9,8 @@ import commands.CommandResponse;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CommandReceiver {
     private final CollectionManager collectionManager;
@@ -21,19 +23,21 @@ public class CommandReceiver {
     public CommandResponse help(String[] args, Object obj) {
         StringBuilder stringBuilder = new StringBuilder();
         AbstractCommand[] commands = (AbstractCommand[]) obj;
-        for (AbstractCommand command : commands) {
-            if (!command.getName().equals("save")) {
-                stringBuilder.append(command.getName()).append(" - ").append(command.describe()).append("\n");
-            }
-            //stringBuilder.append(command.getName()).append(" - ").append(command.describe()).append("\n");
-        }
+        Arrays.stream(commands)
+                .filter(command -> !command.getName().equals("save"))
+                .forEach(command -> stringBuilder.append(command.getName()).append(" - ").append(command.describe()).append("\n"));
+
         stringBuilder.append("help - ").append("shows help for available commands");
         return new CommandResponse(stringBuilder.toString(), null);
     }
 
     public CommandResponse update(String[] args, Object obj) {
         int key = Integer.parseInt(args[0]);
-        if (!collectionManager.getHumanBeingCollection().containsKey(key)) {
+        Optional<HumanBeing> optionalHumanBeing = collectionManager.getHumanBeingCollection().values().stream()
+                .filter(human -> human.getId() == key)
+                .findFirst();
+
+        if (optionalHumanBeing.isEmpty()) {
             return new CommandResponse("Element with this key doesn't exist.", null);
         }
         HumanBeing humanBeing = (HumanBeing) obj;
@@ -52,17 +56,29 @@ public class CommandReceiver {
     public CommandResponse replaceIfLower(String[] args, Object obj) {
         //check if key exists
         int key = Integer.parseInt(args[0]);
-        if (!collectionManager.getHumanBeingCollection().containsKey(key)) {
-            return new CommandResponse("Key doesn't exist.", null);
-        }
-        HumanBeing humanBeing = (HumanBeing) obj;
-        humanBeing.setId(key);
-        if (collectionManager.getHumanBeingCollection().get(key).compareTo(humanBeing) > 0) {
-            collectionManager.insert(key, humanBeing);
-            return new CommandResponse("Element changed successfully.", null);
-        } else {
-            return new CommandResponse("Element is not lower.", null);
-        }
+//
+//        HumanBeing humanBeing = (HumanBeing) obj;
+//        humanBeing.setId(key);
+//        if (collectionManager.getHumanBeingCollection().get(key).compareTo(humanBeing) > 0) {
+//            collectionManager.insert(key, humanBeing);
+//            return new CommandResponse("Element changed successfully.", null);
+//        } else {
+//            return new CommandResponse("Element is not lower.", null);
+//        }
+        return collectionManager.getHumanBeingCollection().containsKey(key)
+                ? Optional.ofNullable((HumanBeing) obj)
+                .map(humanBeing -> {
+                    humanBeing.setId(key);
+                    if (collectionManager.getHumanBeingCollection().get(key).compareTo(humanBeing) > 0) {
+                        collectionManager.insert(key, humanBeing);
+                        return new CommandResponse("Element changed successfully.", null);
+                    } else {
+                        return new CommandResponse("Element is not lower.", null);
+                    }
+                })
+                .orElse(new CommandResponse("Invalid object.", null))
+                : new CommandResponse("Key doesn't exist.", null);
+
     }
 
     public CommandResponse removeGreater(String[] args, Object obj) {
@@ -71,33 +87,39 @@ public class CommandReceiver {
     }
 
     public CommandResponse remove(String[] args, Object obj) {
-
         int id = Integer.parseInt(args[0]);
-        if (collectionManager.removeById(id)) {
-            return new CommandResponse("Element with id " + id + " was removed", null);
-        } else {
-            return new CommandResponse("Element with id " + id + " wasn't found", null);
-        }
+        boolean isRemoved = collectionManager.removeById(id);
+
+        String message = isRemoved ? "Element with id " + id + " was removed" : "Element with id " + id + " wasn't found";
+
+        return new CommandResponse(message, null);
 
     }
 
     public CommandResponse printDesc(String[] args, Object obj) {
-        try {
-            ArrayList<HumanBeing> elements = new ArrayList<>(List.of(collectionManager.getArray()));
-            elements.sort(Comparator.reverseOrder());
-            return new CommandResponse("Elements in descending order", elements);
-
-        } catch (Exception e) {
-            return new CommandResponse("Something went wrong", null);
-        }
+//        ArrayList<HumanBeing> elements = new ArrayList<>(List.of(collectionManager.getArray()));
+//        elements.sort(Comparator.reverseOrder());
+//        return new CommandResponse("Elements in descending order", elements);
+        List<HumanBeing> elements = Arrays.stream(collectionManager.getArray())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        return new CommandResponse("Elements in descending order", elements);
     }
 
     public CommandResponse insert(String[] args, Object obj) {
         //console.println(args[0] + args[1]);
         int key = Integer.parseInt(args[0]);
-        if (collectionManager.getHumanBeingCollection().containsKey(key)) {
+//        if (collectionManager.getHumanBeingCollection().containsKey(key)) {
+//            return new CommandResponse("Element with this key already exists.", null);
+//        }
+        Optional<HumanBeing> optionalHumanBeing = collectionManager.getHumanBeingCollection().values().stream()
+                .filter(human -> human.getId() == key)
+                .findFirst();
+
+        if (optionalHumanBeing.isPresent()) {
             return new CommandResponse("Element with this key already exists.", null);
         }
+
         HumanBeing humanBeing = null;
         humanBeing = (HumanBeing) obj;
         humanBeing.setId(key);
@@ -114,8 +136,12 @@ public class CommandReceiver {
     }
 
     public CommandResponse show(String[] args, Object obj) {
-        ArrayList<HumanBeing> coll = new ArrayList<>(List.of(collectionManager.getArray()));
-        return new CommandResponse("Showed all elements of collection", coll);
+        //ArrayList<HumanBeing> coll = new ArrayList<>(List.of(collectionManager.getArray()));
+        List<HumanBeing> elements = Arrays.stream(collectionManager.getArray())
+                .sorted(Comparator.reverseOrder())
+                .toList();
+
+        return new CommandResponse("Showed all elements of collection", elements);
     }
 
     public CommandResponse clear(String[] args, Object object) {
@@ -124,65 +150,88 @@ public class CommandReceiver {
     }
 
     public CommandResponse countGreaterThanCar(String[] args, Object obj) {
-        int count = 0;
         Car car = (Car) obj;
-        for (HumanBeing humanBeing : collectionManager.getHumanBeingCollection().values()) {
-            if (humanBeing.getCar().compareTo(car) > 0) count++;
-        }
+        int count = (int) Arrays.stream(collectionManager.getArray())
+                .filter(humanBeing -> humanBeing.getCar().compareTo(car) > 0)
+                .count();
         return new CommandResponse("There are " + count + " elements whose car field value is greater than the specified one", null);
     }
 
     public CommandResponse groupCountingByImpact(String[] args, Object obj) {
+//        HumanBeing[] elements = collectionManager.getArray();
+//        int[] impactSpeed = new int[elements.length];
+//        for (int i = 0; i < elements.length; i++) {
+//            impactSpeed[i] = elements[i].getImpactSpeed().intValue();
+//        }
+//        Map<Integer, Integer> counter = new HashMap<>();
+//
+//        for (int j : impactSpeed) {
+//            if (counter.containsKey(j)) {
+//                counter.replace(j, counter.get(j) + 1);
+//            } else {
+//                counter.put(j, 1);
+//            }
+//        }
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+//            stringBuilder.append("Impact speed: ")
+//                    .append(entry.getKey())
+//                    .append(" - ")
+//                    .append(entry.getValue())
+//                    .append(" elements")
+//                    .append(System.lineSeparator());
+//        }
+//
+//        String result = stringBuilder.toString();
         HumanBeing[] elements = collectionManager.getArray();
-        int[] impactSpeed = new int[elements.length];
-        for (int i = 0; i < elements.length; i++) {
-            impactSpeed[i] = elements[i].getImpactSpeed().intValue();
-        }
-        Map<Integer, Integer> counter = new HashMap<>();
+        int[] impactSpeed = Arrays.stream(elements)
+                .mapToInt(humanBeing -> humanBeing.getImpactSpeed().intValue())
+                .toArray();
 
-        for (int j : impactSpeed) {
-            if (counter.containsKey(j)) {
-                counter.replace(j, counter.get(j) + 1);
-            } else {
-                counter.put(j, 1);
-            }
-        }
-        StringBuilder stringBuilder = new StringBuilder();
+        Map<Integer, Integer> counter = Arrays.stream(impactSpeed)
+                .boxed()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        value -> 1,
+                        Integer::sum
+                ));
 
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            stringBuilder.append("Impact speed: ")
-                    .append(entry.getKey())
-                    .append(" - ")
-                    .append(entry.getValue())
-                    .append(" elements")
-                    .append(System.lineSeparator());
-        }
-
-        String result = stringBuilder.toString();
+        String result = counter.entrySet().stream()
+                .map(entry -> "Impact speed: " + entry.getKey() + " - " + entry.getValue() + " elements")
+                .collect(Collectors.joining(System.lineSeparator()));
         return new CommandResponse("Grouped counting by impact speed entries", result);
-
-
     }
 
     public CommandResponse history(String[] args, Object obj) {
-        StringBuilder commands = new StringBuilder();
-        String message = "Last 13 commands:";
+//        StringBuilder commands = new StringBuilder();
+//        String message = "Last 13 commands:";
         CommandManager commandManager = (CommandManager) obj;
-        for (String command : commandManager.getCommandHistory()) {
-            commands.append(command).append("\n");
-        }
-        return new CommandResponse(message, commands.toString());
+//        for (String command : commandManager.getCommandHistory()) {
+//            commands.append(command).append("\n");
+//        }
+
+        String commands = Arrays.stream(commandManager.getCommandHistory().toArray())
+                .map(Object::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+        return new CommandResponse("Last 13 commands: ", commands);
     }
 
     public CommandResponse connect(String[] args, Object obj) {
         CommandManager commandManager = (CommandManager) obj;
         AbstractCommand[] commands = commandManager.getCommandsArray();
-        ArrayList<CommandDescription> commandDescriptions = new ArrayList<>();
-        for (AbstractCommand command : commands) {
-            if (!command.getName().equals("save")) {
-                commandDescriptions.add(CommandDescriptionFactory.createCommandDescription(command.getClass()));
-            }
-        }
+//        ArrayList<CommandDescription> commandDescriptions = new ArrayList<>();
+//        for (AbstractCommand command : commands) {
+//            if (!command.getName().equals("save")) {
+//                commandDescriptions.add(CommandDescriptionFactory.createCommandDescription(command.getClass()));
+//            }
+//        }
+        List<CommandDescription> commandDescriptions = Arrays.stream(commands)
+                .filter(command -> !command.getName().equals("save"))
+                .map(command -> CommandDescriptionFactory.createCommandDescription(command.getClass()))
+                .collect(Collectors.toList());
+
         return new CommandResponse("Got commands", commandDescriptions);
     }
 }
